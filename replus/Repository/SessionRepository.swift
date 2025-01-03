30deb1a05edeba56f5ec157da6cf23b7c2718caa
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-03 17:57:21 +1000

    Add create Session code in SessionRepository file

--- replus/Repository/SessionRepository.swift
+++ replus/Repository/SessionRepository.swift
@@ -5,3 +5,30 @@
 //  Created by Joshua Delos Santos on 3/1/2025.
 //
 
+
+import CoreData
+
+
+class SessionRepository {
+    private let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    /// Create a new Session.
+    func createSession(id: UUID = UUID(), name: String) throws {
+        let newSession = Session(context: context)
+        newSession.id = id
+        newSession.name = name
+        
+        try saveContext()
+        }
+    
+    /// Save context changes.
+    private func saveContext() throws {
+        if context.hasChanges {
+            try context.save()
+        }
+    }
+}
